c5f16e188569a27686cc2f4dcb1f5d55971b4211
jainigo <joseantonio.inigo@gmail.com>
2023-04-18 14:59:01 +0200

    fail test

--- src/test/kotlin/ServiceOneTests.kt
+++ src/test/kotlin/ServiceOneTests.kt
@@ -2,12 +2,15 @@ import MockitoHelper.anyObject
 import MockitoHelper.eq
 import arrow.core.Either
 import arrow.core.raise.either
+import com.codependent.cr.BusinessError
+import com.codependent.cr.BusinessError.SomeError
 import com.codependent.cr.ServiceOne
 import com.codependent.cr.ServiceTwo
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito
+import org.mockito.Mockito.*
 
 
 class ServiceOneTests {
@@ -15,11 +18,11 @@ class ServiceOneTests {
     @Test
     fun `should return the correct aggregation result`() {
 
-        val serviceTwo = Mockito.mock(ServiceTwo::class.java)
+        val serviceTwo = mock(ServiceTwo::class.java)
         val serviceOne = ServiceOne(serviceTwo)
 
         val result = either {
-            Mockito.`when`(serviceTwo::call.invoke(anyObject(), eq("1"))).thenReturn("1")
+            `when`(serviceTwo::call.invoke(anyObject(), eq("1"))).thenReturn("1")
             val mockResult = serviceTwo.call("1")
             assertEquals("1", mockResult)
 
@@ -32,11 +35,30 @@ class ServiceOneTests {
         }
     }
 
+    @Test
+    fun `should fail`() {
+        val serviceTwo = mock(ServiceTwo::class.java)
+        val serviceOne = ServiceOne(serviceTwo)
+
+        val result = either {
+            `when`(serviceTwo::call.invoke(anyObject(), eq("1")))
+                .thenAnswer {
+                    raise(SomeError)
+                }
+            serviceOne.call("1")
+        }
+
+        when(result){
+            is Either.Left -> println("OK")
+            is Either.Right -> fail()
+        }
+    }
+
 }
 
 object MockitoHelper {
     fun <T> anyObject(): T {
-        Mockito.any<T>()
+        any<T>()
         return uninitialized()
     }
 
